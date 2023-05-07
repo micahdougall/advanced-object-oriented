33d509fe6c0008dc8c75f62813894fbe3a7fb303
Micah Dougall <micahd@tutamail.com>
2023-05-07 14:33:33 +0100

    Fixed pointer references in loop

--- coursework/src/trie.c
+++ coursework/src/trie.c
@@ -5,8 +5,37 @@ struct Trie {
 	product_t *product;
 	int test;
 };
-// struct Tile *tries;
 typedef struct Trie trie_node;
+
+
+
+void print_node(trie_node *node, char *route) {
+	if (route == NULL) {
+		printf("\nStarting at root node...\n\n");
+	} else {
+		printf("Printing for route %s...\n", route);
+	}
+
+	printf("%s -> %d\n", route, node -> test);
+
+	// TODO: Use size_t for safety
+	for (int i = 0; i < 10; i++) {
+		char int_str[2];
+
+		if (node -> children[i]) {
+			sprintf(int_str, "%d", i);
+
+			if (route == NULL) {
+				route = int_str;
+			} else {
+				strcat(route, int_str);
+			}
+			printf("Child found at route: %s\n", route);
+			print_node(node -> children[i], route);
+		}
+	}
+}
+
 
 // void insert_into_trie(trie_node *root_node, product *product) {
 void insert_into_trie(unsigned int code) {
@@ -15,43 +44,73 @@ void insert_into_trie(unsigned int code) {
 	char code_as_str[(int) log10(code) + 1];
 	sprintf(code_as_str, "%i", code);
 
-	printf("%s\n", code_as_str);
+	printf("Code as string: %s\n", code_as_str);
 
-	trie_node **child_ptrs = malloc(sizeof(trie_node *) * 10); 
+	// Create root node
+	trie_node root_node = { 
+		.children = malloc(sizeof(trie_node *) * 10)
+	};
+	printf(" -> root_node is %p\n", &root_node);
 
-	trie_node root_trie = { .children = child_ptrs };
-	trie_node *child_node;
+	trie_node *current_node_ptr = &root_node;
+	printf(" -> current_node_ptr is %p\n", current_node_ptr);
 
+	trie_node *new_node = (trie_node *) malloc(sizeof(trie_node *));
 
-	for (unsigned short int i = 0; i < strlen(code_as_str); i++) {
-		printf("i = %u\n", i);
+	for (unsigned short int c = 0; c < strlen(code_as_str); c++) {
+		char code_char = code_as_str[c];
+		unsigned short int idx = atoi(&code_char);
 
-		char c = code_as_str[i];
+		printf("\n\nLooking for child: %u\n", idx);
 
-		printf("c = %c\n", c);
+		// Assign pointer to the relevant index
+		// trie_node *child_node = current_node_ptr -> children[idx];
 
-
-		unsigned short int digit = atoi(&c);
-
-		printf("Looking for child: %u\n", digit);
-
-
-		if (root_trie.children[digit]) {
-			printf("Child exists: %u\n", digit);
-			child_node = root_trie.children[digit];
+		if (current_node_ptr -> children[idx]) {
+			printf("Child exists: %u\n", idx);
 		} else {
-			printf("Child [%u] doesn't exist\n", digit);
-			trie_node child = { .test = 9 };
-			root_trie.children[digit] = &child;
-		}
-		printf("child node test = %u\n", root_trie.children[digit] -> test);
-	}
+			printf("Child [%u] doesn't exist\n", idx);
+			printf(" -> child_node is %p\n", current_node_ptr -> children[idx]);
 
-	for (int i = 0; i < 10; i++) {
-		if (root_trie.children[i]) {
-			printf("%i\n", root_trie.children[i] -> test);
-		} else {
-			printf("Child [%i] doesn't exist\n", i);
+
+
+			// Create new child node
+			// new_node.
+
+			trie_node **children = malloc(sizeof(trie_node *) * 10);
+
+			trie_node *new_node = malloc(sizeof(trie_node *));
+			new_node -> children = children;
+
+			current_node_ptr -> children[idx] = new_node;
+			// current_node_ptr -> children[idx] = *new_node;
+			printf(" -> child_node is %p\n", current_node_ptr -> children[idx]);
+
+
+			// *(new_node -> children) = malloc(sizeof(trie_node *) * 10);
+
+			// trie_node new_node = { 
+				// .children = malloc(sizeof(trie_node *) * 10) 
+			// };
+			printf(" -> new_node is %p\n", new_node);
+			printf(" -> new_node is %p\n", &new_node);
+
+			// char test[10];
+			// printf(" -> test is %p\n", &test);
+
+			// child_node = &new_node;
+			*(current_node_ptr -> children[idx]) = *new_node;
+			printf("Added new child at branch %u\n", idx);
+			printf(" -> child_node is %p\n", current_node_ptr -> children[idx]);
 		}
+
+		current_node_ptr = current_node_ptr -> children[idx];
+		printf(" -> current_node_ptr is %p\n", current_node_ptr);
+
 	}
+	// For proof of tree work where product would be
+	current_node_ptr -> test = 10;
+
+	printf(" -> root_node is %p\n", &root_node);
+	print_node(&root_node, NULL);
 }
